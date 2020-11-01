b6509b5c4603bc50ba7fe40dc3af0a43b34d5c66
Leon Leitao <leitaoleon@gmail.com>
2020-11-01 23:51:27 +0100

    sorted options in alphabetical order

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import pandas as pd
 
 all_players=pd.read_csv('data/predictions.csv').to_dict('records')
 current_gw=all_players[0]['event']
+top6=all_players[:6]
+all_players = sorted(all_players, key=lambda k: k['first_name']) 
 
 app = Flask(__name__)
 
@@ -18,8 +20,8 @@ def predict():
     if request.method=='POST':
         player_id=request.form.get('code')
         player_selected=next(player for player in all_players if player["code"] == int(player_id))
-        return render_template('predict.html',title='Points predictor',players=all_players,cgw=int(current_gw),player_id=player_id,player_selected=player_selected)
-    return render_template('predict.html',title='Points predictor',players=all_players,cgw=int(current_gw),player_id=player_id)
+        return render_template('predict.html',title='Points predictor',players=all_players,cgw=int(current_gw),top6=top6,player_id=player_id,player_selected=player_selected)
+    return render_template('predict.html',title='Points predictor',players=all_players,cgw=int(current_gw),top6=top6,player_id=player_id)
 
 if __name__=='__main__':
     app.run()
